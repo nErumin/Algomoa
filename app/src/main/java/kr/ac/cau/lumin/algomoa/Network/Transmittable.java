b766b27fa528833d08a45b3a05c162392b3c9964
nErumin <nerumin90@gmail.com>
2015-11-21 21:00:50 +0900

    Add interface 'Transmittable'.

---
+++ app/src/main/java/kr/ac/cau/lumin/algomoa/Network/Transmittable.java
@@ -0,0 +1,9 @@
+package kr.ac.cau.lumin.algomoa.Network;
+
+/**
+ * Created by CAUCSE on 2015-11-21.
+ */
+public interface Transmittable {
+    public static String SPERATOR = "/";
+    public String getRequestURL();
+}
---
+++ app/src/main/java/kr/ac/cau/lumin/algomoa/Util/LanguageList.java
@@ -0,0 +1,8 @@
+package kr.ac.cau.lumin.algomoa.Util;
+
+/**
+ * Created by Lumin on 2015-11-21.
+ */
+public enum LanguageList {
+    Ruby, Java
+}
