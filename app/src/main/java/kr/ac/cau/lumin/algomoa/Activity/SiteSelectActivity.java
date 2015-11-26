3dd18591f81aa954df61894706763242e40ee2b3
nErumin <nerumin90@gmail.com>
2015-11-26 21:29:07 +0900

    Doing adding listview in navigation view...

--- app/src/main/java/kr/ac/cau/lumin/algomoa/Activity/SiteSelectActivity.java
+++ app/src/main/java/kr/ac/cau/lumin/algomoa/Activity/SiteSelectActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 
 import kr.ac.cau.lumin.algomoa.R;
 
