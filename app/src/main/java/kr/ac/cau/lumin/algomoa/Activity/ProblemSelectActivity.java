4389c04e7d15685bcb1bd6ae9b3bb2b1a4848464
nErumin <nerumin90@gmail.com>
2015-11-26 21:48:54 +0900

    Implement Reference view activity.

---
+++ app/src/main/java/kr/ac/cau/lumin/algomoa/Activity/ProblemSelectActivity.java
@@ -0,0 +1,52 @@
+package kr.ac.cau.lumin.algomoa.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import kr.ac.cau.lumin.algomoa.R;
+
+/**
+ * Created by Lumin on 2015-11-26.
+ */
+public class ProblemSelectActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_view);
+
+        this.initializeToolBar();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void initializeToolBar() {
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (this.toolbar != null) {
+            this.setSupportActionBar(this.toolbar);
+            this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
--- app/src/main/java/kr/ac/cau/lumin/algomoa/Activity/ReferenceViewActivity.java
+++ app/src/main/java/kr/ac/cau/lumin/algomoa/Activity/ReferenceViewActivity.java
@@ -1,7 +1,10 @@
 package kr.ac.cau.lumin.algomoa.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -16,30 +19,52 @@ import kr.ac.cau.lumin.algomoa.Util.SimpleWebViewClient;
  */
 public class ReferenceViewActivity extends AppCompatActivity {
     WebView referWebView;
-    Button backButton;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String languageName = this.getIntent().getExtras().getString("lang_name");
-        String className = this.getIntent().getExtras().getString("class_name");
-        String targetURL;
+        //String languageName = this.getIntent().getExtras().getString("lang_name");
+        //String className = this.getIntent().getExtras().getString("class_name");
+        //String targetURL = "";
 
         super.onCreate(savedInstanceState);
         // TODO : What?
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_webview);
 
-        targetURL = AlgomoaSQLHelper.getInstance(this).getReferenceURL(LanguageList.valueOf(languageName), className);
-        //this.referWebView = (WebView) findViewById(R.id.);
-        //this.backButton = (Button) findViewById(R.id.);
-        this.referWebView.getSettings().setJavaScriptEnabled(true);
+        this.initializeToolBar();
+        //targetURL = AlgomoaSQLHelper.getInstance(this).getReferenceURL(LanguageList.valueOf(languageName), className);
+        this.referWebView = (WebView) findViewById(R.id.refer_webview);
         this.referWebView.setWebViewClient(new SimpleWebViewClient());
-        this.referWebView.loadUrl(targetURL);
+        this.referWebView.getSettings().setJavaScriptEnabled(true);
+        this.referWebView.loadUrl("http://www.google.com");
+    }
 
-        this.backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO : Go to Previous Activity
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, ReferenceSearchActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void initializeToolBar() {
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar_refer_view);
+
+        if (this.toolbar != null) {
+            this.setSupportActionBar(this.toolbar);
+            this.getSupportActionBar().setDisplayUseLogoEnabled(false);
+            this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
